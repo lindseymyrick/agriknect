b6bfdf29c34c298879c99eb16ff9b834a79fca23
Lindsey Myrick <lindsey.k.myrick@gmail.com>
2020-07-09 17:35:56 -0500

    Allis Chalmers changed

--- database.sql
+++ database.sql
@@ -1,98 +1,92 @@
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "first_name" VARCHAR (50) NOT NULL,
-    "last_name" VARCHAR (50) NOT NULL,
-    "username" VARCHAR (100) UNIQUE NOT NULL,
-    "password" VARCHAR (150) NOT NULL,
-    "type" VARCHAR (50)
+  "id" SERIAL PRIMARY KEY,
+  "first_name" VARCHAR (50) NOT NULL,
+  "last_name" VARCHAR (50) NOT NULL,
+  "username" VARCHAR (100) UNIQUE NOT NULL,
+  "password" VARCHAR (200) NOT NULL,
+  "type" VARCHAR (50),
+  "form_complete" BOOLEAN DEFAULT FALSE
 );
-
 CREATE TABLE "talent_user" (
 	"id" SERIAL PRIMARY KEY,
-    "bio" VARCHAR (1000),
-    "city" VARCHAR (100),
-    "state" VARCHAR (100),
-    "zipcode" INTEGER,
-    "latitude" DECIMAL,
-    "longitude" DECIMAL,
-    "user_id" INT REFERENCES "user"
+  "bio" VARCHAR (1000),
+  "city" VARCHAR (100),
+  "state" VARCHAR (100),
+  "zipcode" INTEGER,
+  "latitude" DECIMAL,
+  "longitude" DECIMAL,
+  "user_id" INT
 );
-
 CREATE TABLE "farm" (
-    "id" SERIAL PRIMARY KEY,
-    "farm_name" VARCHAR (1000),
-    "street_address" VARCHAR (200),
-    "city" VARCHAR (100),
-    "state" VARCHAR (100),
-    "zipcode" INTEGER,
-    "phone" VARCHAR,
-    "size" VARCHAR,
-    "type" VARCHAR (50),
-    "bio" VARCHAR (1000),
-    "latitude" DECIMAL,
-    "longitude" DECIMAL,
-    "user_id" INT REFERENCES "user"
+  "id" SERIAL PRIMARY KEY,
+  "farm_name" VARCHAR (1000),
+  "street_address" VARCHAR (200),
+  "city" VARCHAR (100),
+  "state" VARCHAR (100),
+  "zipcode" INTEGER,
+  "phone" VARCHAR,
+  "size" VARCHAR,
+  "type" VARCHAR (50),
+  "bio" VARCHAR (1000),
+  "latitude" DECIMAL,
+  "longitude" DECIMAL,
+  "user_id" INT
 );
 CREATE TABLE "certification" (
 	"id" SERIAL PRIMARY KEY,
-    "certification_name" VARCHAR (100),
-    "issuing_company" VARCHAR (100),
-    "issue_date" DATE,
-    "expiration_date" DATE,
-    "user_id" INT REFERENCES "user"
+  "certification_name" VARCHAR (100),
+  "issuing_company" VARCHAR (100),
+  "issue_date" DATE,
+  "expiration_date" DATE,
+  "user_id" INT
 );
-
 CREATE TABLE "employment" (
 	"id" SERIAL PRIMARY KEY,
-    "employer_name" VARCHAR (100),
-    "title" VARCHAR (100),
-    "start_date" DATE,
-    "end_date" DATE,
-    "user_id" INT REFERENCES "user"
+  "employer_name" VARCHAR (100),
+  "title" VARCHAR (100),
+  "start_date" DATE,
+  "end_date" DATE,
+  "user_id" INT
 );
-
 CREATE TABLE "jobs" (
 	"id" SERIAL PRIMARY KEY,
-    "user_id" INT,
-    "title" VARCHAR (100),
-    "description" VARCHAR (100),
-    "type" VARCHAR (100),
-    "start_date" DATE,
-    "end_date" DATE,
-    "housing" VARCHAR(50),
-    "housing_details" VARCHAR (100),
-    "relocation_stipend" VARCHAR(50),
-    "payment_period" VARCHAR (100),
-    "payment_amount" INT
+  "user_id" INT,
+  "title" VARCHAR (100),
+  "description" VARCHAR (10000),
+  "type" VARCHAR (100),
+  "start_date" DATE,
+  "end_date" DATE,
+  "housing" BOOLEAN,
+  "housing_details" VARCHAR (100),
+  "relocation_stipend" BOOLEAN,
+  "payment_period" VARCHAR (100),
+  "payment_amount" INT
 );
-
 CREATE TABLE "proficiencies" (
 	"id" SERIAL PRIMARY KEY,
-    "proficiency_name" VARCHAR (50),
-    "proficiency_category" VARCHAR (100)
+  "proficiency_name" VARCHAR (50),
+  "proficiency_category" VARCHAR (100)
 );
-
 CREATE TABLE "job_proficiencies" (
 	"id" SERIAL PRIMARY KEY,
-    "proficiency_id" INTEGER REFERENCES "proficiencies",
-    "job_id" INT 
+  "proficiency_id" INTEGER,
+  "job_id" INT
 );
-
 CREATE TABLE "user_proficiencies" (
 	"id" SERIAL PRIMARY KEY,
-    "proficiency_id" INT REFERENCES "proficiencies",
-    "length_experience" VARCHAR (100),
-    "user_id" INTEGER REFERENCES "user"
+  "proficiency_id" INT,
+  "length_experience" VARCHAR (100),
+  "user_id" INTEGER
 );
-
 CREATE TABLE "education" (
 	"id" SERIAL PRIMARY KEY,
-    "institution_name" VARCHAR (100),
-    "degree" VARCHAR (100),
-    "start_date" DATE,
-    "end_date" DATE,
-    "user_id" INT REFERENCES "user"
+  "institution_name" VARCHAR (100),
+  "degree" VARCHAR (100),
+  "start_date" DATE,
+  "end_date" DATE,
+  "user_id" INT
 );
+
 
 -- proficiencies data
 INSERT INTO "proficiencies" ("proficiency_name", "proficiency_category")
@@ -104,7 +98,7 @@ VALUES
 ('Massey Fergueson', 'Brand'),
 ('Claas', 'Brand'),
 ('Kubota', 'Brand'),
-('Alias Chalmers', 'Brand'),
+('Allis-Chalmers', 'Brand'),
 ('John Deere Autotrac', 'Precision Farming Technology'),
 ('Case IH AFS AccuGuide', 'Precision Farming Technology'),
 ('Ag Leader Steer Command', 'Precision Farming Technology'),
